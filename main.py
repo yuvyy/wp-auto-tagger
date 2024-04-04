d08c704a2cb1c0039012d9b48136884e4df0c040
unknown <yuvydarekar@gmail.com>
2024-04-04 13:01:24 +0530

    format code

--- main.py
+++ main.py
@@ -2,9 +2,12 @@ import pyautogui
 import time
 
 
-x = int(input("Enter number of participants in the group (including you)"))
-time.sleep(5)
+x = int(input("Enter number of participants in the group (including you): "))
 print('Open whatsapp chatbox')
+for i in range(5,0,-1):
+    print('Tagging starting in: {} sec'.format(i))
+    time.sleep(1)
+    
 
 pyautogui.hotkey('shift', '2')
 # pyautogui.press('down')
@@ -16,3 +19,5 @@ for _ in range(x-2):
     pyautogui.press('down')
     pyautogui.press('enter')
     time.sleep(0.3)
+
+print("Tagging complete press enter to send the tagged message")
